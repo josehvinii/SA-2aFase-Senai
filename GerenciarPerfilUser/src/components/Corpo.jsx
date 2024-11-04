542d31b9f84fc116dbf47b0c42eb858083609e82
JOSÉ VINÍCIUS VICTOR DAUZAKER <jose_dauzaker@estudante.sesisenai.org.br>
2024-11-04 17:18:58 -0300

    TelaUser4.0

--- GerenciarPerfilUser/src/components/Corpo.jsx
+++ GerenciarPerfilUser/src/components/Corpo.jsx
@@ -5,19 +5,25 @@ function Corpo() {
   return (
     <div className='Corpo-container'>
 
-     <h1 className='dadosDaConta'>Dados da Conta</h1>
+      <h1 className='dadosDaConta'>Dados da Conta</h1>
 
-     <div className='divUm'>
+      <div className='divUm'>
 
-      <div className='divDois'>
+        <div className='divDois'>
+          <img className='iconUser' src="./img/iconUser.png" />
+          <label className='alterarfoto'>Alterar foto</label>
+          <label className='labelInpts'>Nome</label> <input className='inpts' type="text" />
+          <label className='labelInpts'>Email</label><input className='inpts' type="text" />
+          <label className='labelInpts'>Senha</label><input className='inpts' type="text" />
+        </div>
 
-      <img className='iconUser' src="./img/iconUser.png"/>
-      <label className='labelInpts'>Nome</label> <input className='inpts' type="text" />
-      <label className='labelInpts'>Email</label><input className='inpts' type="text" />
-      <label className='labelInpts'>Senha</label><input className='inpts' type="text" />
+        <div className='botoes'>
+          <button className='botao'>EXCLUIR CONTA</button>
+          <button className='botao'>DESCONECTAR</button>
+        </div>
+
       </div>
 
-     </div>
 
     </div>
   )
