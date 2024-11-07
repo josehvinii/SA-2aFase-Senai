c4d4790456c28cc59cf81429e84874b33705c25e
JOSÉ VINÍCIUS VICTOR DAUZAKER <jose_dauzaker@estudante.sesisenai.org.br>
2024-11-07 17:22:13 -0300

    telaUserLegaozona

--- GerenciarPerfilUser/src/components/Corpo.jsx
+++ GerenciarPerfilUser/src/components/Corpo.jsx
@@ -1,38 +1,39 @@
-import React from 'react'
-import './Corpo.css'
+import React from 'react';
+import './Corpo.css';
 
 function Corpo() {
   return (
     <div className='Corpo-container'>
-
       <h1 className='dadosDaConta'>Dados da Conta</h1>
-
       <div className='divUm'>
-
         <div className='divDois'>
-          <img className='iconUser' src="./img/fotoUser.png" />
-          <label className='alterarfoto'>Alterar foto <img className='iconeFoto' src="./img/iconFoto.png"/></label>
+          <img className='iconUser' src="./img/fotoUser.png" alt="User Icon" />
+          <label className='alterarfoto'>
+            Alterar foto <img className='iconeFoto' src="./img/iconFoto.png" alt="Change Photo Icon"/>
+          </label>
 
-          <label className='labelInpts'>Nome</label>      
-          <input className='inpts' type="text" /> <img className='iconeLapis' src="./img/iconLapis.png"/>
+          <label className='labelInpts'>Nome</label>
+          <div className='inptLapis'>
+            <input className='input-nome' type="text"/>
+            <span className="icon-Lapis">
+              <img className='iconeLapis' src="./img/iconLapis.png" alt="Edit Icon"/>
+            </span>
+          </div>
 
           <label className='labelInpts'>Email</label>
-          <input className='inpts' type="text" />
+          <input className='inpts' type="text"/>
 
           <label className='labelInpts'>Senha</label>
-          <input className='inpts' type="text" />
+          <input className='inpts' type="password"/>
         </div>
 
         <div className='botoes'>
           <button className='botao'>EXCLUIR CONTA</button>
           <button className='botao'>DESCONECTAR</button>
         </div>
-
       </div>
-
-
     </div>
-  )
+  );
 }
 
-export default Corpo
+export default Corpo;
