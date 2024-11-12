6ad603e498041f441066e3afe182014a8e12ebaf
JOSÉ VINÍCIUS VICTOR DAUZAKER <jose_dauzaker@estudante.sesisenai.org.br>
2024-11-12 17:21:27 -0300

    telaGerenciamento4.0

--- GerenciarPerfilUser/src/components/Corpo.jsx
+++ GerenciarPerfilUser/src/components/Corpo.jsx
@@ -4,59 +4,44 @@ import './Corpo.css';
 function Corpo() {
   return (
     <div className='Corpo-container'>
-      <div className="espaco">
-        <h1 className='dadosDaConta'>Dados da Conta</h1>
-        <div className="espaco2">
-          <h1 className='especificacoes'>Especificações</h1>
-        </div>
-
-      </div>
-      <div className='divUm'>
-        <div className='divDois'>
+      
+        <div className='divUm'>
+          <h1 className='dadosDaConta'>Dados da Conta</h1>
           <img className='iconUser' src="./img/fotoUser.png" alt="User Icon" />
           <label className='alterarfoto'>
-            Alterar foto <img className='iconeFoto' src="./img/iconFoto.png" alt="Change Photo Icon" />
+            alterar foto <img className='iconeFoto' src="./img/iconFoto.png" alt="Change Photo Icon" />
           </label>
 
-          <label className='labelInpts'>Nome</label>
-          <div className='inptLapis'>
-            <input className='input-nome' type="text" />
-            <span className="icon-Lapis">
-              <img className='iconeLapis' src="./img/iconLapis.png" alt="Edit Icon" />
-            </span>
-          </div>
+          <label className='labelInpts'>Nome</label> 
+          <input className='inpts' type="text" value="alguma pessoa da silva" />
 
-          <label className='labelInpts'>Email</label>
-          <input className='inpts' type="text" />
+          <label className='labelInpts'>E-mail</label>
+          <input className='inpts' type="text" value="AlgumaPessoaDaSilva@gmail.com" />
 
           <label className='labelInpts'>Senha</label>
-          <input className='inpts' type="password" />
+          <input className='inpts' type="password" value="algum@_pesso@_d@_silv@123" /> <br/>
 
-          <div className='botoes'>
-            <button className='botao'>EXCLUIR CONTA</button>
-            <button className='botao'>DESCONECTAR</button>
-          </div>
+            <button className='excluir'>EXCLUIR A CONTA</button>
+            <button className='sair'>Sair da conta</button>
+          
         </div>
 
         <div className="divLinha"></div>
 
         <div className='divDois'>
-
+          <h1 className='especificacoes'>Especificações</h1>
           <div className='divCheckBox'>
-          <label className='labelInpts'>Possui alguma deficiência?</label>
-          <label><input className='checkbox' type="checkbox" /> Sim </label>
-          <label><input className='checkbox' type="checkbox"/> Não</label>
+            <label className='labelInpts'>Possui alguma deficiência?</label>
+            <label><input className='checkbox' type="radio" name="deficiencia" checked /> Sim</label>
+            <label><input className='checkbox' type="radio" name="deficiencia" /> Não</label>
           </div>
 
-          <label className='labelInpts'>Deficiência</label>
-          <input className='inpts' type="text" />
-          <label className='labelInpts'>Detalhes</label>
-          <input className='inpts' type="text" />
+          <label className='labelInpts'>Deficiência:</label>
+          <input className='inpts' type="text" value="Autismo" />
+
+          <label className='labelInpts'>Detalhes:</label>
+          <input className='inpts' type="text" value="blablabla..." />
         </div>
-
-
-
-      </div>
     </div>
   );
 }
