508f46fa09a84a48f6eb22d5b7ebea32111c0220
JOSÉ VINÍCIUS VICTOR DAUZAKER <jose_dauzaker@estudante.sesisenai.org.br>
2024-11-18 17:21:38 -0300

    telaGerenciamento6.0

--- GerenciarPerfilUser/src/components/Corpo.jsx
+++ GerenciarPerfilUser/src/components/Corpo.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import './Corpo.css';
 
+
 function Corpo() {
   return (
     <div className='Corpo-container'>
-      
+     
         <div className='divUm'>
           <h1 className='dadosDaConta'>Dados da Conta</h1>
           <img className='iconUser' src="./img/fotoUser.png" alt="User Icon" />
@@ -12,40 +13,52 @@ function Corpo() {
             alterar foto <img className='iconeFoto' src="./img/iconFoto.png" alt="Change Photo Icon" />
           </label>
 
-          <label className='labelInpts'>Nome</label> 
+
+          <img className='verificado' src="./img/img logo.png" alt="" />
+
+
+          <label className='labelInpts'>Nome</label>
           <input className='inpts' type="text"/>
+
 
           <label className='labelInpts'>E-mail</label>
           <input className='inpts' type="text"/>
 
+
           <label className='labelInpts'>Senha</label>
           <input className='inpts' type="password"/> <br/>
 
+
             <button className='sair'>Sair da conta</button>
             <button className='excluir'>EXCLUIR A CONTA</button>
-          
+         
         </div>
 
+
         <div className="divLinha"></div>
+
 
         <div className='divDois'>
           <h1 className='especificacoes'>Especificações</h1>
 
-          <div className='divCheckBox'>
-            <label className='possuiDeficiencia'>Possui alguma deficiência?</label>
-            <label><input className='checkbox' type="radio" name="deficiencia"/> Sim</label>
-            <label><input className='checkbox' type="radio" name="deficiencia" /> Não</label>
-          </div>
 
-          <label className='labelInpts'>Deficiência:</label>
+            <label className='possuiDeficiencia'>Possui alguma deficiência?</label> <img className='verificado2' src="./img/img logo.png" alt="" />
+            <input type="checkbox" className='checkBox'/> <label className='sim'>Sim</label>
+            <input type="checkbox" className='checkBox'/> <label className='nao'>Não</label>
+
+
+          <label className='deficiencia'>Deficiência:</label>
           <input className='inptsTela2' type="text" />
 
-          <label className='labelInpts'>Detalhes:</label>
+
+          <label className='detalhes'>Detalhes:</label>
           <input className='inptsTela2' type="text" />
         </div>
+
 
     </div>
   );
 }
+
 
 export default Corpo;
