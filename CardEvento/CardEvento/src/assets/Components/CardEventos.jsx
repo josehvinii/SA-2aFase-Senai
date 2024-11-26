f52888daafbca4dee4f13a2a7a4d26af47f2a4c3
JOSÉ VINÍCIUS VICTOR DAUZAKER <jose_dauzaker@estudante.sesisenai.org.br>
2024-11-26 14:49:14 -0300

    CardEvento4.0

--- CardEvento/CardEvento/src/assets/Components/CardEventos.jsx
+++ CardEvento/CardEvento/src/assets/Components/CardEventos.jsx
@@ -2,12 +2,15 @@ import React from 'react'
 import './CardEventos.css'
 
 
+
+
 function CardEvento() {
   return (
     <div className='container-card'>
       <div className="card">
         <img className='imagemEvento' src="./Img/Img-Evento.PNG"/>
-        <div className="card-body">
+        <div className="div-inform">
+          <h2 className='descricao'>27º Congresso Internacional UNIDAS - Inovação impulsionando o propósito das autogestões</h2>
           <h2 className='data'>27 nov - 2024 • 08:00 {'>'} 29 nov - 2024 • 19:00</h2>
           <p className='local'>CentroSul Florianópolis, Florianópolis - SC</p>
         </div>
@@ -15,6 +18,8 @@ function CardEvento() {
     </div>
   )
 }
+
+
 
 
 export default CardEvento
