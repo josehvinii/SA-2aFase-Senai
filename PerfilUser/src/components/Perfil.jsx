0fdad52b739e2865da2005ca20ecf4d925c49eb5
JOSÉ VINÍCIUS VICTOR DAUZAKER <jose_dauzaker@estudante.sesisenai.org.br>
2024-12-02 16:10:04 -0300

    perfilUser3.0

--- PerfilUser/src/components/Perfil.jsx
+++ PerfilUser/src/components/Perfil.jsx
@@ -11,8 +11,17 @@ function Perfil() {
   <p className='nomeUser'>Juliana Peres</p>
 
   <label className='possuiDeficiencia'>Possui alguma deficiência?</label> <img className='verificado2' src="./img/img logo.png" alt="" />
-  <input type="checkbox" className='checkBox' /> <label className='sim'>Sim</label>
-  <input type="checkbox" className='checkBox' /> <label className='nao'>Não</label>
+  
+  <div className="checkBoxGroup">
+  <div>
+    <input type="checkbox" className="checkBox" id="sim" />
+    <label htmlFor="sim">Sim</label>
+  </div>
+  <div>
+    <input type="checkbox" className="checkBox" id="nao" />
+    <label htmlFor="nao">Não</label>
+  </div>
+</div>
 
 
   <label className='deficiencia'>Deficiência:</label>
@@ -31,9 +40,20 @@ function Perfil() {
 
 <div className='divDois'>
   
-  <h1 className='especificacoes'>Especificações</h1>
+  <h1 className='Frase1'>Mantenha suas informações seguras!</h1>
 
-  <button className='sair'>Sair da conta</button>
+  <p className='Texto1'>Evite compartilhar seus dados pessoais com outras pessoas. 
+    Suas informações são protegidas e usadas apenas para 
+    melhorar sua experiência.</p>
+
+    <h1 className='Frase2'>Atualização é importante!</h1>
+  
+  <p className='Texto2'>Mantenha suas informações atualizadas para garantir uma 
+  experiência mais personalizada e eficiente.</p>
+
+  <button className='gerenciar'>Gerenciar Perfil</button>
+  <img className='iconGerenciar' src="./img/image 47.png"/>
+  
   
 </div>
 
